5c5e7e70a752d32fa89a2283cd446da92e84777d
Ilya Evteev <evteevil6@gmail.com>
2022-04-18 13:37:03 +0500

    better start

--- src/Map.tsx
+++ src/Map.tsx
@@ -20,7 +20,11 @@ const Map = ({data, mapData, dispatch}: MapProps) => {
 
         if (!mapData)
         {
-            var map = L.map('map');
+            var map = L.map('map', {
+                center: L.latLng(51.505, -0.09),
+                zoom: 13
+              });
+              
             L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
                 maxZoom: 18,
